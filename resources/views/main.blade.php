8bd8cdbbb433f38aafeef1346e25976cb664ca07
hunter <f882032002@yahoo.com.tw>
2018-09-16 00:32:55 +0800

    1. header , footer 建置

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="/js/plugins/AOS/aos.css">
-
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Brush+Script" rel="stylesheet">
     <!-- Styles -->
     <style>
         .paytest {
@@ -57,14 +57,199 @@
 
 <!------------------------header------------------------>
 
-123
+<section id="header">
+
+    <div class="header_top_area">
+        <div class="header_left">
+            <div class="logo_area">
+                <div class="logo">
+                    <div class="mark">
+                        <img src="/img/king.svg" alt="">
+                        <p class="mark_text mark_top">
+                            KLS
+                        </p>
+                        <p class="mark_text mark_middle">
+                            凱麗絲旅行箱專賣店
+                        </p>
+                        <p class="mark_text mark_bottom">
+                            KLS Baggage Department Store.
+                        </p>
+                    </div>
+                    <div class="time">
+                        <img src="/img/time.svg" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="header_contact">
+                <div class="header_addr">
+                    <div class="shop_list">
+                        <div class="shop zhubei">
+                            <img src="\img\thumbtack.svg" alt="">
+                            <div class="shop_contact">
+                                <p>竹北店 (09:00 - 20:00)</p>
+                                <p>03-6565528</p>
+                                <p>新竹縣竹北市光明六路15號</p>
+                            </div>
+                        </div>
+                        <div class="shop bigcity">
+                            <img src="\img\thumbtack.svg" alt="">
+                            <div class="shop_contact">
+                                <p>巨城店 (09:00 - 20:00)</p>
+                                <p>03-5326191</p>
+                                <p>新竹市民權路173號</p>
+                            </div>             
+                        </div>
+                        <div class="shop guanxin">
+                            <img src="\img\thumbtack.svg" alt="">
+                            <div class="shop_contact">
+                                <p>關新店 (09:00 - 22:00)</p>
+                                <p>03-5638999</p>
+                                <p>新竹市關新路201號</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="community">
+                    <div class="facebook">
+                        <div class="link">
+                            <img src="\img\facebook.svg" alt="">
+                            <p>
+                                facebook
+                            </p>
+                        </div>
+                    </div>
+                    <div class="instagram">
+                        <div class="link">
+                            <img src="\img\instagram.svg" alt="">
+                            <p>
+                                instagram
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header_right">
+            <div class="banner">
+                <div class="banner_word">
+                    <h1>
+                        我在LA機場的人來人往...
+                    </h1>
+                    <p>
+                        #長途旅行專用行李箱
+                    </p>
+                    <p>
+                        #30吋/29吋熱賣中
+                    </p>
+                    <p>
+                        PC硬殼箱
+                    </p>
+                    <p class="eng">
+                        10285 Post Way,Los Angeles,LAX AirPort,洛杉磯機場
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="navbar">
+        <ul>
+            <li>
+                <a href="">
+                    關於凱麗絲
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    品牌行李箱                   
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    行李箱配件
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    部落格
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    線上拍賣
+                </a>
+            </li>
+            <li>
+                <a href="">
+                   聯絡我們
+                </a>
+            </li>
+        </ul>
+    </div>
+    
+</section>
 
 
 @yield('content')
 
 <!------------------------footer------------------------>
 
+<section id="footer">
+    <div class="footer_logo">
 
+    </div>
+    <div class="footer_contact">
+        <div class="footer_addr">
+            <div class="shop_list">
+                <div class="shop zhubei">
+                    <img src="\img\thumbtack.svg" alt="">
+                    <div class="shop_contact">
+                        <p>竹北店 (09:00 - 20:00)</p>
+                        <p>03-6565528</p>
+                        <p>新竹縣竹北市光明六路15號</p>
+                    </div>
+                </div>
+                <div class="shop bigcity">
+                    <img src="\img\thumbtack.svg" alt="">
+                    <div class="shop_contact">
+                        <p>巨城店 (09:00 - 20:00)</p>
+                        <p>03-5326191</p>
+                        <p>新竹市民權路173號</p>
+                    </div>             
+                </div>
+                <div class="shop guanxin">
+                    <img src="\img\thumbtack.svg" alt="">
+                    <div class="shop_contact">
+                        <p>關新店 (09:00 - 22:00)</p>
+                        <p>03-5638999</p>
+                        <p>新竹市關新路201號</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="community">
+            <div class="facebook">
+                <div class="link">
+                    <img src="\img\facebook.svg" alt="">
+                    <p>
+                        facebook
+                    </p>
+                </div>
+            </div>
+            <div class="instagram">
+                <div class="link">
+                    <img src="\img\instagram.svg" alt="">
+                    <p>
+                        instagram
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer_copy">
+
+    </div>
+</section>
 
 <!-- Scripts -->
 <script src="{{ asset('js/frontend.js') }}"></script>
@@ -87,6 +272,56 @@
 @if(config('app.env') == 'local')
     <script src="http://localhost:35729/livereload.js"></script>
 @endif
+
+<script>
+
+    // heights
+    $(document).ready(() => {
+
+        function get_H(){
+
+            let h = $(window).height()
+            let logo_h = $('.logo').height()
+            let nav_h = $('.navbar').height()
+
+            $('#header').css('height',h)
+            
+        };
+        get_H()
+
+        $(window).resize(() => {
+            get_H()
+        });
+    });
+
+    $(function() {
+        try {
+            var fix = $('.navbar');                      //title
+            var fixTop = fix.offset().top,              //title 與頂部的距離
+                fixHeight = fix.height();               //title 高度
+           
+    
+            $(window).scroll(() => {
+                // 頁面與頂部高度
+                var docTop = Math.max(document.body.scrollTop, document.documentElement.scrollTop);  
+                if (fixTop < docTop) {
+                    fix.css("position","fixed");
+                    fix.css("top",0);
+                    fix.css("width","100%");
+                } else {
+                    fix.css("position","relative");
+                    }
+                })
+        } catch (error) {
+            console.log(error.message)
+        }
+        
+    });
+
+
+                  
+        
+</script>
 
 </body>
 </html>
