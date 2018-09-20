7143d5980720ae13b9b93125d5dbe746b213efa1
hunter <f882032002@yahoo.com.tw>
2018-09-21 00:12:09 +0800

    建構index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,129 @@
 
 @section('content')
 
-    <!------------------------banner------------------------>
+    <!------------------------choose------------------------>
+
+    <section id="choose">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 choose_title">
+                   <p> 
+                       挑選適合你的行李箱
+                   </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 co_post">
+                    <div class="choose_post">
+                        <div class="img_div">
+                            <img src="/img/choose01.jpg" alt="">
+                        </div>
+                        <div class="post_text">
+                            <h2>
+                                拉鍊式硬殼箱
+                            </h2>
+                            <p>
+                                鋁合金特性是耐用、耐磨、耐衝擊、箱
+                                殼本身的使用年限大多可以保持在五年
+                                或十年以上，有分一體成型，如ZERO，
+                                或組合成型、如RIMOWA。但周邊的配
+                                件損壞還是可以維修的，如果你要求外
+                                觀漂亮完整，大概不可能，除非，是您
+                                想換個新的行李箱，要不然，要使用到
+                                不堪使用的情況，應該是很少發生的、
+                                但前提是、必須在正確使用行李箱的方
+                                法之下、才能發揮它的特性、他跟一般
+                                行李箱的比較來說，重量重一點價格比
+                                一般的還要貴很多。
+                            </p>
+                        </div>
+                        <button class ="goBuy">點我選購去</button>
+                    </div>
+                </div>
+                <div class="col-md-3 co_post">
+                    <div class="choose_post">
+                        <div class="img_div">
+                            <img src="/img/choose02.jpg" alt="">
+                        </div>
+                        <div class="post_text">
+                            <h2>
+                                鋁框式硬殼箱
+                            </h2>
+                            <p>
+                                鋁合金特性是耐用、耐磨、耐衝擊、箱
+                                殼本身的使用年限大多可以保持在五年
+                                或十年以上，有分一體成型，如ZERO，
+                                或組合成型、如RIMOWA。但周邊的配
+                                件損壞還是可以維修的，如果你要求外
+                                觀漂亮完整，大概不可能，除非，是您
+                                想換個新的行李箱，要不然，要使用到
+                                不堪使用的情況，應該是很少發生的、
+                                但前提是、必須在正確使用行李箱的方
+                                法之下、才能發揮它的特性、他跟一般
+                                行李箱的比較來說，重量重一點價格比
+                                一般的還要貴很多。
+                            </p>
+                        </div>
+                        <button class ="goBuy">點我選購去</button>
+                    </div>
+                </div>
+                <div class="col-md-3 co_post">
+                    <div class="choose_post">
+                        <div class="img_div">
+                            <img src="/img/choose03.jpg" alt="">
+                        </div>
+                        <div class="post_text">
+                            <h2>
+                                軟殼布面箱
+                            </h2>
+                            <p>
+                                鋁合金特性是耐用、耐磨、耐衝擊、箱
+                                殼本身的使用年限大多可以保持在五年
+                                或十年以上，有分一體成型，如ZERO，
+                                或組合成型、如RIMOWA。但周邊的配
+                                件損壞還是可以維修的，如果你要求外
+                                觀漂亮完整，大概不可能，除非，是您
+                                想換個新的行李箱，要不然，要使用到
+                                不堪使用的情況，應該是很少發生的、
+                                但前提是、必須在正確使用行李箱的方
+                                法之下、才能發揮它的特性、他跟一般
+                                行李箱的比較來說，重量重一點價格比
+                                一般的還要貴很多。
+                            </p>
+                        </div>
+                        <button class ="goBuy">點我選購去</button>
+                    </div>
+                </div>
+                <div class="col-md-3 co_post">
+                    <div class="choose_post">
+                        <div class="img_div">
+                            <img src="/img/choose04.jpg" alt="">
+                        </div>
+                        <div class="post_text">
+                            <h2>
+                                品牌特殊箱
+                            </h2>
+                            <p>
+                                鋁合金特性是耐用、耐磨、耐衝擊、箱
+                                殼本身的使用年限大多可以保持在五年
+                                或十年以上，有分一體成型，如ZERO，
+                                或組合成型、如RIMOWA。但周邊的配
+                                件損壞還是可以維修的，如果你要求外
+                                觀漂亮完整，大概不可能，除非，是您
+                                想換個新的行李箱，要不然，要使用到
+                                不堪使用的情況，應該是很少發生的、
+                                但前提是、必須在正確使用行李箱的方
+                                法之下、才能發揮它的特性、他跟一般
+                                行李箱的比較來說，重量重一點價格比
+                                一般的還要貴很多。
+                            </p>
+                        </div>
+                        <button class ="goBuy">點我選購去</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
     <!------------------------count------------------------>
 
